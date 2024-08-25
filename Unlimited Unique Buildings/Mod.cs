358cf88790881902e74b9d8202c63c1c895740ef
rodrigmatrix <rodrigmatrix@gmail.com>
2024-08-25 17:53:42 -0300

    mod release

---
+++ Unlimited Unique Buildings/Mod.cs
@@ -0,0 +1,26 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace UnlimitedUniqueBuildings
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(UnlimitedUniqueBuildings)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+            updateSystem.UpdateAt<UnlimitedUniqueBuildingsSystem>(SystemUpdatePhase.PrefabUpdate);
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+    }
+}
---
+++ Unlimited Unique Buildings/UnlimitedUniqueBuildingsSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+using Game.Prefabs;
+using Game;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace UnlimitedUniqueBuildings
+{
+    public partial class UnlimitedUniqueBuildingsSystem : GameSystemBase
+    {
+        private Dictionary<Entity, PlaceableObjectData> _placebleData = new Dictionary<Entity, PlaceableObjectData>();
+
+        private EntityQuery _query;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            _query = GetEntityQuery(new EntityQueryDesc()
+            {
+                All = [
+                    ComponentType.ReadWrite<PlaceableObjectData>()
+                ]
+            });
+
+            RequireForUpdate(_query);
+        }
+
+        protected override void OnUpdate()
+        {
+            var buildings = _query.ToEntityArray(Allocator.Temp);
+
+            foreach (var building in buildings)
+            {
+                PlaceableObjectData data;
+
+                if (!_placebleData.TryGetValue(building, out data))
+                {
+                    data = EntityManager.GetComponentData<PlaceableObjectData>(building);
+                    _placebleData.Add(building, data);
+                }
+                data.m_Flags &= ~Game.Objects.PlacementFlags.Unique;
+                EntityManager.SetComponentData(building, data);
+            }
+        }
+    }
+}
+

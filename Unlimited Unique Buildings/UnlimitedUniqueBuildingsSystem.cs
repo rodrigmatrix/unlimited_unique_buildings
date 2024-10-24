c470798a6ac50fcd5b997a4db6ca0af64eab25ab
rodrigmatrix <rodrigmatrix@gmail.com>
2024-10-23 21:40:08 -0300

    release 1.0.3

--- Unlimited Unique Buildings/UnlimitedUniqueBuildingsSystem.cs
+++ Unlimited Unique Buildings/UnlimitedUniqueBuildingsSystem.cs
@@ -1,55 +1,43 @@
-﻿using System.Collections.Generic;
-
-using Game.Prefabs;
+﻿using Game.Prefabs;
 using Game;
 using Unity.Collections;
 using Colossal.Serialization.Entities;
 using Unity.Entities;
-using Colossal.Logging;
 using System;
+using Colossal.Entities;
 
 namespace UnlimitedUniqueBuildings
 {
     public partial class UnlimitedUniqueBuildingsSystem : GameSystemBase
     {
-        private Dictionary<Entity, PlaceableObjectData> _placebleData = new Dictionary<Entity, PlaceableObjectData>();
-
-        private EntityQuery _query;
-
-        public static ILog log = LogManager.GetLogger($"{nameof(UnlimitedUniqueBuildings)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
-
-        protected override void OnCreate()
-        {
-            base.OnCreate();
-        }
-
+        
         protected override void OnGameLoadingComplete(Purpose purpose, GameMode mode)
         {
             try
             {
-                _query = GetEntityQuery(new EntityQueryDesc()
+                var query = GetEntityQuery(new EntityQueryDesc()
                 {
                     All = [
                         ComponentType.ReadWrite<PlaceableObjectData>()
                     ]
                 });
-                var buildings = _query.ToEntityArray(Allocator.Temp);
+                var buildings = query.ToEntityArray(Allocator.Temp);
 
                 foreach (var building in buildings)
                 {
                     PlaceableObjectData data;
 
-                    if (!_placebleData.TryGetValue(building, out data))
+                    if (EntityManager.TryGetComponent(building, out data))
                     {
-                        data = EntityManager.GetComponentData<PlaceableObjectData>(building);
-                        _placebleData.Add(building, data);
+                        data.m_Flags &= ~Game.Objects.PlacementFlags.Unique;
+                        EntityManager.SetComponentData(building, data);
                     }
-                    data.m_Flags &= ~Game.Objects.PlacementFlags.Unique;
-                    EntityManager.SetComponentData(building, data);
                 }
+
+                buildings.Dispose();
             } catch(Exception e)
             {
-                log.Warn($"Error updating Unlimited Unique Buildings " + e);
+                Mod.log.Warn($"Error updating Unlimited Unique Buildings " + e);
             }
         }
 
